16a52f65b5c7562c71f1f70d2d4260ea7bc6d099
xetnal <santiago.deaguirre@gmail.com>
2022-05-05 16:32:03 +0000

    finished

---
+++ src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+todos = [
+    {'label': 'My First Task', 'done': False},
+    
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request.data)
+    print('Incoming request with the following body', decoded_object)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
